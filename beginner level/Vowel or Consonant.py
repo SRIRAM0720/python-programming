70e953ed48c9cdf1edd246e9f7814317b5007e5e
SRIRAM0720 <37569591+SRIRAM0720@users.noreply.github.com>
2018-03-21 10:07:16 +0530

    Update Vowel or Consonant.py

--- beginner level/Vowel or Consonant.py
+++ beginner level/Vowel or Consonant.py
@@ -1,6 +1,6 @@
-a=raw_input("")
-l=['a','e','i','o','u','A','E','I','O','U']
-if(a in l):
+x=raw_input("")
+v=['a','e','i','o','u','A','E','I','O','U']
+if(x in v):
     print("Vowel")
 else:
     print("Consonant")
