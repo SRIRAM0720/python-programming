b6e3a552d2dd614bb66c4deba1a2089abe5140f4
SRIRAM0720 <37569591+SRIRAM0720@users.noreply.github.com>
2018-03-21 10:43:15 +0530

    Update Greatest of three numbers.py

--- beginner level/Greatest of three numbers.py
+++ beginner level/Greatest of three numbers.py
@@ -1,9 +1,16 @@
 a=raw_input("")
 b=raw_input("")
 c=raw_input("")
-if(b<a>c):
-    print("a is Greater")
-elif(a<b>c):
-    print("b is Greater")
-else:
-    print("c is greater")
+if isinstance(a,str) and isinstance(b,str) and isinstance(c,str):
+    if(a.isalpha()) and (b.isalpha()) and (c.isalpha()):
+        print("Invalid input")
+    else:
+        a=int (a)
+        b=int (b)
+        c=int (c)
+        if(b<a>c):
+            print("a is Greater")
+        elif(a<b>c):
+            print("b is Greater")
+        else:
+            print("c is greater")
