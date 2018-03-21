29c9d2b90fbfbe1f36ff5d8aeaeab7589727822e
SRIRAM0720 <37569591+SRIRAM0720@users.noreply.github.com>
2018-03-21 12:02:08 +0530

    Update leap year or not.py

--- beginner level/leap year or not.py
+++ beginner level/leap year or not.py
@@ -1,5 +1,9 @@
-a=int(raw_input(""))
-if(a%400==0):
-    print("Leap year")
+a=raw_input()
+if a.isdigit():
+    a=int(a)
+    if(a%4==0 and a%100!=0 or a%400==0):
+        print("yes")
+    else:
+        print("no")
 else:
-    print("Not Leap year")
+    print("Invalid Input")
