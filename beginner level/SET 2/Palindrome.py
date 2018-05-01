193f13676368bc425e1ee5fc3ad8b505edd3283a
SRIRAM0720 <37569591+SRIRAM0720@users.noreply.github.com>
2018-05-01 10:32:30 +0530

    Update Palindrome.py

--- beginner level/SET 2/Palindrome.py
+++ beginner level/SET 2/Palindrome.py
@@ -1,1 +1,9 @@
-
+ num=input('Enter a number')
+  try:
+    val=int(num)
+    if num == str(num)[::-1]:
+      print('The given number is Palindrome')
+    else:
+      print('The given number is not a palindrome')
+except ValueError:
+  print("That's not a valid number")
